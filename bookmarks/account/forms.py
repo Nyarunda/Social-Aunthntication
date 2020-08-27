883885b374bc5296a67d4ba716dd1d4fa895d631
nyarunda <ezranyagaka96@gmail.com>
2020-08-27 11:56:33 +0300

    first commit

---
+++ bookmarks/account/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'input-text'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'input-text'}))
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password', widget=forms.PasswordInput(attrs={'class': 'input-text'}))
+    password2 = forms.CharField(label='Repeate Password', widget=forms.PasswordInput(
+        attrs={'class': 'input-text'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match. ')
+        return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
